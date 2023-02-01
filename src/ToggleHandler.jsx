9bad3e6fac4494d8751afc7d572eb2dc0db3fe2c
Hedita <hedi.takrami@gmail.com>
2023-02-01 23:21:06 +0330

    add toggle file

---
+++ src/ToggleHandler.jsx
@@ -0,0 +1,200 @@
+import React, { useState } from "react";
+
+export const ToggleHandler0 = () => {
+  const [toggle, setToggle] = useState("off");
+
+  const handleClick = () => {
+    setToggle(!toggle);
+  };
+  return (
+    <div
+      onClick={handleClick}
+      style={{
+        width: 50,
+        height: 50,
+        background: toggle ? "red" : "blue",
+      }}
+    >
+    </div>
+  );
+};
+export const ToggleHandler1 = () => {
+  const [toggle, setToggle] = useState("off");
+
+  const handleClick = () => {
+    setToggle(!toggle);
+  };
+  return (
+    <div
+      onClick={handleClick}
+      style={{
+        width: 50,
+        height: 50,
+        background: toggle ? "red" : "blue",
+      }}
+    >
+    </div>
+  );
+};
+export const ToggleHandler2 = () => {
+  const [toggle, setToggle] = useState("off");
+
+  const handleClick = () => {
+    setToggle(!toggle);
+  };
+  return (
+    <div
+      onClick={handleClick}
+      style={{
+        width: 50,
+        height: 50,
+        background: toggle ? "red" : "blue",
+      }}
+    >
+    </div>
+  );
+};
+export const ToggleHandler3 = () => {
+  const [toggle, setToggle] = useState("off");
+
+  const handleClick = () => {
+    setToggle(!toggle);
+  };
+  return (
+    <div
+      onClick={handleClick}
+      style={{
+        width: 50,
+        height: 50,
+        background: toggle ? "red" : "blue",
+      }}
+    >
+    </div>
+  );
+};
+export const ToggleHandler4 = () => {
+  const [toggle, setToggle] = useState("off");
+
+  const handleClick = () => {
+    setToggle(!toggle);
+  };
+  return (
+    <div
+      onClick={handleClick}
+      style={{
+        width: 50,
+        height: 50,
+        background: toggle ? "red" : "blue",
+      }}
+    >
+    </div>
+  );
+};
+export const ToggleHandler5 = () => {
+  const [toggle, setToggle] = useState("off");
+
+  const handleClick = () => {
+    setToggle(!toggle);
+  };
+  return (
+    <div
+      onClick={handleClick}
+      style={{
+        width: 50,
+        height: 50,
+        background: toggle ? "red" : "blue",
+      }}
+    >
+    </div>
+  );
+};
+export const ToggleHandler6 = () => {
+  const [toggle, setToggle] = useState("off");
+
+  const handleClick = () => {
+    setToggle(!toggle);
+  };
+  return (
+    <div
+      onClick={handleClick}
+      style={{
+        width: 50,
+        height: 50,
+        background: toggle ? "red" : "blue",
+      }}
+    >
+    </div>
+  );
+};
+export const ToggleHandler7 = () => {
+  const [toggle, setToggle] = useState("off");
+
+  const handleClick = () => {
+    setToggle(!toggle);
+  };
+  return (
+    <div
+      onClick={handleClick}
+      style={{
+        width: 50,
+        height: 50,
+        background: toggle ? "red" : "blue",
+      }}
+    >
+    </div>
+  );
+};
+export const ToggleHandler8 = () => {
+  const [toggle, setToggle] = useState("off");
+
+  const handleClick = () => {
+    setToggle(!toggle);
+  };
+  return (
+    <div
+      onClick={handleClick}
+      style={{
+        width: 50,
+        height: 50,
+        background: toggle ? "red" : "blue",
+      }}
+    >
+    </div>
+  );
+};
+export const ToggleHandler9 = () => {
+  const [toggle, setToggle] = useState("off");
+
+  const handleClick = () => {
+    setToggle(!toggle);
+  };
+  return (
+    <div
+      onClick={handleClick}
+      style={{
+        width: 50,
+        height: 50,
+        background: toggle ? "red" : "blue",
+      }}
+    >
+    </div>
+  );
+};
+export const ToggleHandler10 = () => {
+  const [toggle, setToggle] = useState("off");
+
+  const handleClick = () => {
+    setToggle(!toggle);
+  };
+  return (
+    <div
+      onClick={handleClick}
+      style={{
+        width: 50,
+        height: 50,
+        background: toggle ? "red" : "blue",
+      }}
+    >
+    </div>
+  );
+};
