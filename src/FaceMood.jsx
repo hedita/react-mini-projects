e87f2b7a3d8a844c57f8ddb2f06ab62717ef119b
Hedita <hedi.takrami@gmail.com>
2023-02-02 11:50:58 +0330

    add file

---
+++ src/FaceMood.jsx
@@ -0,0 +1,30 @@
+const people = [
+    {
+      name: "Siamak",
+      mood: "sad"
+    },
+    {
+      name: "Ati",
+      mood: "happy"
+    },
+    {
+      name: "Hedi",
+      mood: "interested"
+    },
+    {
+      name: "Soheyl",
+      mood: "happy"
+    },
+    {
+      name: "Shamim",
+      mood: "excited"
+    },
+    {
+      name: "Ali",
+      mood: "interested"
+    },
+    {
+      name: "Karen",
+      mood: "nice"
+    }
+  ]
