43cbe2ba31c55b390952fdb792b8fc3b611d613a
Hedita <hedi.takrami@gmail.com>
2023-02-02 11:42:51 +0330

    add component

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import InputHandler from "./InputHandler";
 import InputTextHandler from "./InputTextHandler";
 import CounterHandler from "./CounterHandler";
 import LampHandler from "./LampHandler";
-import { ToggleHandler0, ToggleHandler1,ToggleHandler2,ToggleHandler3,ToggleHandler4,ToggleHandler5,ToggleHandler6,ToggleHandler7,ToggleHandler8,ToggleHandler9,ToggleHandler10 } from "./ToggleHandler";
+import ToggleHandler from "./ToggleHandler";
 
 const App = () => {
   return (
@@ -16,27 +16,7 @@ const App = () => {
       <hr />
       <LampHandler />
       <hr />
-      <ToggleHandler0 />
-      <br />
-      <ToggleHandler1 />
-      <br />
-      <ToggleHandler2 />
-      <br />
-      <ToggleHandler3 />
-      <br />
-      <ToggleHandler4 />
-      <br />
-      <ToggleHandler5 />
-      <br />
-      <ToggleHandler6 />
-      <br />
-      <ToggleHandler7 />
-      <br />
-      <ToggleHandler8 />
-      <br />
-      <ToggleHandler9 />
-      <br />
-      <ToggleHandler10 />
+      <ToggleHandler />
     </>
   );
 };
