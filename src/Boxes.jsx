ba83e9faece51f79e2e61bf0bb41312f7ba9af98
Hedita <hedi.takrami@gmail.com>
2023-02-05 12:44:01 +0330

    remove the ul and li

--- src/Boxes.jsx
+++ src/Boxes.jsx
@@ -1,17 +1,15 @@
-import React from "react";
+import React, { Fragment } from "react";
 import Box from "./Box";
 
 function Boxes() {
   const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
   const listItem = numbers.map((number) => (
-    <>
-      <li>
-        <Box />
-      </li>
+    <Fragment key={number}>
+      <Box />
       <br />
-    </>
+    </Fragment>
   ));
-  return <ul>{listItem}</ul>;
+  return listItem;
 }
 
 export default Boxes;
