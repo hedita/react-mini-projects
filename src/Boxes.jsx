baa0b77649a392f1783c172df8977f14148d905d
Hedita <hedi.takrami@gmail.com>
2023-02-05 11:52:51 +0330

    format the code

--- src/Boxes.jsx
+++ src/Boxes.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import Box from "./Box";
 
-function Boxes () {
-  const numbers = [0,1,2,3,4,5,6,7,8,9];
-  const listItem = numbers.map((number) =>
+function Boxes() {
+  const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const listItem = numbers.map((number) => (
     <>
-      <li> <Box /> </li>
+      <li>
+        <Box />
+      </li>
       <br />
     </>
-  )
-  return (
-    <ul>{listItem}</ul>
-  )
+  ));
+  return <ul>{listItem}</ul>;
 }
 
 export default Boxes;
