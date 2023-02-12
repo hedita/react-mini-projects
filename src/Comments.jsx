0941aa748f8bbddcec47f1f6a524dd2bc0783126
Hedita <hedi.takrami@gmail.com>
2023-02-12 22:01:15 +0330

    fix comment visibility

--- src/Comments.jsx
+++ src/Comments.jsx
@@ -1,22 +1,29 @@
-import React, {useState,Fragment} from "react";
+import React, { useState, Fragment } from "react";
 
-function Comments ({comments}) {
-  console.log(comments)
+function Comments({ comments }) {
   const [isCommentsVisible, setIsCommentsVisible] = useState(false);
   const handleCommentVisibility = () => {
     setIsCommentsVisible(!isCommentsVisible);
-  }
-  const commentList = comments.map(({author,text,likesCount,id}) =>{
-    return (
-      <Fragment key={id}>
-       <div style={{visibility: isCommentsVisible ? "visible" : "hidden"}}>
-         <p>{author}:{text}</p>
-         <p>likes: {likesCount}</p>
-       </div>
-       <button onClick={handleCommentVisibility}>{isCommentsVisible ? "Hide Comments" : "Show Comments"}</button>
-      </Fragment> 
-    ) 
-  })
-  return {commentList}
+  };
+
+  return (
+    <>
+      <button onClick={handleCommentVisibility}>
+        {isCommentsVisible ? "Hide Comments" : "Show Comments"}
+      </button>
+      {isCommentsVisible
+        ? comments.map(({ id, author, text, likesCount }) => {
+            return (
+              <Fragment key={id}>
+                <p>
+                  {author}:{text}
+                </p>
+                <p>likes: {likesCount}</p>
+              </Fragment>
+            );
+          })
+        : null}
+    </>
+  );
 }
 export default Comments;
